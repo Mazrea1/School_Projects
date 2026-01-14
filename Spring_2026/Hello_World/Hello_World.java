beff116b4c2fc04a4ee05a882526e7219517610c
Mazreal23 <michael.lalaj@outlook.com>
2026-01-14 14:56:17 -0600

    First Java Commit

---
+++ Spring_2026/Hello_World/Hello_World.java
@@ -0,0 +1,10 @@
+public class Hello_World {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 5;
+        if (x > y) {
+        System.out.println("Hello world!");
+        System.out.println("x is greater than y");
+        }
+    }
+}
---
+++ Spring_2026/Project1/project1.java
@@ -0,0 +1,7 @@
+public class project1 {
+    public static void main(String[] args) {
+        double radious = 25.05;
+        double area = Math.PI * radious * radious;
+        System.out.println("The area of the circle is: " + area);        
+    }
+}
